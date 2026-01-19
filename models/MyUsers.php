3e4cd93cd12d5125c71f49f9423c22e2063ec4e8
Ghabi Malek <uapv2401806@pedago01c.univ-avignon.fr>
2026-01-19 22:53:45 +0100

    Ajout modèle MyUsers

---
+++ models/MyUsers.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\models;
+
+use yii\db\ActiveRecord;
+
+class MyUsers extends ActiveRecord
+{
+    public static function tableName()
+    {
+       
+        return 'fredouil.my_users';
+    }
+}
