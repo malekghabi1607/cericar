bff2a458274417dd9f3ac63c3b8861d432bcbf10
Ghabi Malek <uapv2401806@pedago01c.univ-avignon.fr>
2026-01-19 22:52:14 +0100

    Modèle TypeVehicule

--- models/TypeVehicule.php
+++ models/TypeVehicule.php
@@ -12,7 +12,7 @@ use yii\db\ActiveRecord;
  *
  * @property Voyage[] $voyages
  */
-class Typevehicule extends ActiveRecord
+class TypeVehicule extends ActiveRecord
 {
     public static function tableName()
     {
